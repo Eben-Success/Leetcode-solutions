cefa9b1fc82863b83a390f9922ce4b0f12cd7ee1
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-11-09 22:04:28 +0000

    Time: 704 ms (54.07%), Space: 31.8 MB (26.16%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -8,9 +8,12 @@ class Solution(object):
         
         temp = ""
         
+        # Convert string into integers
         int_nums = [int(item) for item in nums]
         
+        # Sort integers in descending order
         srt = sorted(int_nums, reverse=True)
         
+        # return the element at the index of k - 1
         return str(srt[k-1])
         
