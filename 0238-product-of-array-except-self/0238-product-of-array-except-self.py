0c4afef98c283464646a36918204645489d7f873
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-11-26 18:23:41 +0000

    Time: 607 ms (38.35%), Space: 21.3 MB (59.41%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,20 +7,17 @@ class Solution:
         
         # Space complexity is O(n)
         
-        res = [1] * (len(nums))
+        res = [1] * len(nums)
         
         prefix = 1
-        
         for i in range(len(nums)):
             res[i] = prefix
             prefix *= nums[i]
-        
+            
         postfix = 1
-        for i in range(len(nums) -1, -1, -1):
+        for i in range(len(nums)-1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
-            
         return res
-        
      
         
