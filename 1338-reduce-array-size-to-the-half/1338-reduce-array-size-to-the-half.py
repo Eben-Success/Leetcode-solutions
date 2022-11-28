5c8b1a5cf5646fad1566656a7125e9eb925ade65
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-11-28 19:58:19 +0000

    Time: 1547 ms (36.44%), Space: 35.7 MB (36.84%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,17 +1,21 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
+        # Use counter to store the occurances of the array in dictionary
+        # sorted the dictionary in descending order.
+        # check if the n >= half of the array
+        
         c = Counter(arr)
         N = len(arr)
+        count = 0
         n = 0
-        output = 0
         
-        # sorting dictionary in descending order
+        # sorts the dictionary in descending order
         for k, v in sorted(c.items(), key=lambda x: -x[1]):
-            n+=v
-            output+= 1
-            if n >= (N // 2): break
-        return output
+            n += v
+            count += 1
             
+            if n >= (N//2): break
+        return count
             
         
         
