83616e05e32916161bbb939a8698896d6663d1d8
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-11-30 21:18:17 +0000

    Time: 72 ms (95.31%), Space: 14.2 MB (45.35%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.py
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.py
@@ -2,17 +2,18 @@ class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         
         # two pointer technique
-        # l on pushed and r on poped
+        # l on pushed and r on popped
         
         r = 0
         stack = []
         
-        for l in pushed:
-            stack.append(l)
+        for l in range(len(pushed)):
+            stack.append(pushed[l])
             
             while stack and r < len(popped) and stack[-1] == popped[r]:
-                r+=1
                 stack.pop()
-        return True if len(stack) == 0 else False
+                r+=1
+        return stack == []
+        
         
     
