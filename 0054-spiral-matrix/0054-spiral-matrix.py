8745fb31c82d876808726d8c8b09afa689c7075f
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-12-03 16:43:47 +0000

    Time: 51 ms (65.64%), Space: 13.9 MB (82.85%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,35 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        # Time complexity:  O(m * n): dimensions of the matrix
+        # Space complexity: O(1)
+        
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+
+            # get every i in the top row
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top +=1
+
+            # get every i in the right col
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            # get every i in the bottom row
+            for i in range(right - 1, left - 1, -1):
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            # get every i in the left most column
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left += 1
+        return res
