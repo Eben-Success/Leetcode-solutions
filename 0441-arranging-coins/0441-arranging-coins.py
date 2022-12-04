8829671830877564ba1d9c756377c665fd63f9d1
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-12-04 09:52:03 +0000

    Time: 39 ms (94.21%), Space: 13.8 MB (96.73%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,18 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        # Using Guass Formular: n/2 (n+1)
+        
+        l, r = 1, n
+        res = 0
+        
+        while l <= r:
+            mid = (l+r) // 2
+            coins = (mid / 2) * (mid + 1)
+            if coins > n:
+                r = mid - 1
+            else:
+                l = mid + 1
+                res = max(mid, res)
+        return res
+        
+        
