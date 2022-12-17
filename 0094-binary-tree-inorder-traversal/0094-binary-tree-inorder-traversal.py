f1a5849f332fdc9d8c0ad686725587ebee6d1c72
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-12-17 11:08:37 +0000

    Time: 37 ms (82.42%), Space: 13.9 MB (59.58%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -10,15 +10,38 @@ class Solution:
         # Traverse left, root, and right
         
         # Time: O(n)
+        # Space: O(n): call stack
+        
+#         res = []
+        
+#         def inorder(root):
+#             if root is None:
+#                 return
+            
+#             inorder(root.left)
+#             res.append(root.val)
+#             inorder(root.right)
+            
+#         inorder(root)
+#         return res
+    
+    
+    
+        # Iterative Solution
+        # Time: O(n)
+        # Space: O(n + m): stack and res
         
         res = []
+        stack = []
+        cur = root
         
-        def inorder(root):
-            if root is None:
-                return
-            inorder(root.left)
-            res.append(root.val)
-            inorder(root.right)
-            
-        inorder(root)
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
         return res
+                
+        
