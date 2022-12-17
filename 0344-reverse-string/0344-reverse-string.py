647661c66800d942724c405b8f6fa79614976523
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-12-17 18:10:04 +0000

    Time: 258 ms (74.85%), Space: 18.7 MB (12.52%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,14 +4,34 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
-        l, r = 0, len(s) - 1
         
-        while l < r:
-            s[l], s[r] = s[r], s[l]
-            l += 1
-            r -= 1
-        return s
+        # Time: O(n)
+        # Space: O(1)
         
+        # Using Two pointers
+        
+#         l, r = 0, len(s) - 1
+        
+#         while l < r:
+#             s[l], s[r] = s[r], s[l]
+#             l += 1
+#             r -= 1
+#         return s
+        
+        # Using Stack
+        # Time: O(n)
+        # Space: O(n)
+        
+        stack = []
+        
+        for char in s:
+            stack.append(char)
+        i = 0
+        while stack:
+            s[i] = stack.pop()
+            i += 1
+            
+            
         
             
         
