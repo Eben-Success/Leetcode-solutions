80e90e01fe2f2a52cd607461bdada9d9b800b4eb
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2022-12-17 14:44:00 +0000

    Time: 56 ms (49.71%), Space: 14 MB (19.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -5,14 +5,13 @@ class Solution:
         # Space: O(n**2)
         
         
-        res = [[1]]
+        res= [[1]]
         
         for i in range(numRows - 1):
             temp = [0] + res[-1] + [0]
-            row = []
+            newRow = []
             
             for j in range(len(res[-1]) + 1):
-                row.append(temp[j] + temp[j+1])
-            res.append(row)
+                newRow.append(temp[j] + temp[j+1])
+            res.append(newRow)
         return res
-                
