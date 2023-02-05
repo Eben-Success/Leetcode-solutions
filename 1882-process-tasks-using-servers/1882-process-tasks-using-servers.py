0033c1e4b426c91ca77b5712d282d13f565d6e5c
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-02-05 22:07:23 +0000

    Time: 2304 ms (54.43%), Space: 55.2 MB (82.54%) - LeetHub

--- 1882-process-tasks-using-servers/1882-process-tasks-using-servers.py
+++ 1882-process-tasks-using-servers/1882-process-tasks-using-servers.py
@@ -1,7 +1,10 @@
 class Solution:
     def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
         
-        # 2nd Optimal approach
+        # Time: O(nlogm)
+        # Space: O(n)
+        
+       
         # two main heaps, one for available server, one for unavailable.
         # available = (weight, index)
         # unavailable = (timeServerBecomesFree, weight, index)
@@ -35,6 +38,33 @@ class Solution:
             heapq.heappush(unavail, (t + tasks[i], weight, index))
 
         return res
+    
+    
+#         #BRUTE FORCE APPROACH
+#         # Time: O(m * n)
+#         # Space: O(n)
+#         n = len(servers)
+#         m = len(tasks)
+        
+#         servers = [(server, i) for i, server in enumerate(servers)]
+#         servers.sort()
+#         ans = [0] * m
+#         q = PriorityQueue()
+#         time = 0
+        
+#         for i in range(m):
+#             while servers and servers[0][0] <= time:
+#                 _, j = servers.pop(0)
+                
+#                 q.put(j)
+#             if q.empty():
+#                 time = servers[0][0]
+                
+#             j = q.get()
+#             ans[i] = j
+#             time += tasks[i]
+#             q.put(j)
+#         return ans
 
 
         
