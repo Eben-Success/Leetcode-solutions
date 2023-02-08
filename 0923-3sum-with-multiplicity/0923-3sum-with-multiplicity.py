a6a4b90dd5bac2d95ae6e00f2148125eeac009f0
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-02-08 22:49:29 +0000

    Time: 4228 ms (23.17%), Space: 14.1 MB (51.07%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
@@ -0,0 +1,14 @@
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        
+        mp, res = {}, 0
+        
+        for i in range(len(arr) - 1):
+            for j in range(i+1, len(arr)):
+                diff = target - (arr[i] + arr[j])
+                res += mp[diff] if diff in mp else 0
+                
+            mp[arr[i]] = mp.get(arr[i], 0) + 1
+            
+        return res % int(1e9 + 7)
+        
