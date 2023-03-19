cbc94613c15bf6d48183098fea43e4652ac975ea
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-03-19 09:52:21 +0000

    Time: 31 ms (85.13%), Space: 14.1 MB (98.06%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -7,16 +7,39 @@
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         
+#         if not root:
+#             return 0
+        
+#         left_sum = 0
+        
+#         # Check if the left child is a leaf node
+#         if root.left and not root.left.left and not root.left.right:
+#             left_sum += root.left.val
+        
+#         left_sum += self.sumOfLeftLeaves(root.left)
+#         left_sum += self.sumOfLeftLeaves(root.right)
+        
+#         return left_sum
+    
+    
         if not root:
             return 0
         
         left_sum = 0
         
-        # Check if the left child is a leaf node
-        if root.left and not root.left.left and not root.left.right:
-            left_sum += root.left.val
+        stack = [(root, False)]
         
-        left_sum += self.sumOfLeftLeaves(root.left)
-        left_sum += self.sumOfLeftLeaves(root.right)
-        
+        while stack:
+            node, is_left = stack.pop()
+            
+            if not node.left and not node.right and is_left:
+                left_sum += node.val
+                
+            if node.right:
+                stack.append((node.right, False))
+                
+            if node.left:
+                stack.append((node.left, True))
+                
         return left_sum
+                
