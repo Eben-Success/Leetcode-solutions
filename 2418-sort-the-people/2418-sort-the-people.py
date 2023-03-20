b5487ffe17f86581202fa8f7d6a09d9bbc339e4d
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-03-20 14:19:39 +0000

    Time: 98 ms (99.92%), Space: 14.4 MB (79.64%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -3,10 +3,13 @@ class Solution:
         
         res = []
         
+        # zip names and height into a List
         combined = list(zip(names, heights))
         
+        # sort the list by the second index
         combined.sort(key=lambda x:x[1], reverse=True)
         
+        # Append the first index to res and return it
         for name in combined:
             res.append(name[0])
         return res
