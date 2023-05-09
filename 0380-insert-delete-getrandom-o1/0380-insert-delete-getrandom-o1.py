b8c9318a9432082d3dd1bc2284272982eb18e19e
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-05-09 19:12:00 +0000

    Time: 381 ms (94.99%), Space: 63.7 MB (5.81%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,32 +1,47 @@
 class RandomizedSet:
+    
+    """
+    {1: 0, 4; 1, 5: 2 }
+    [1, (index 1:10), 5, 7, 9, 10] => remove(4)
+    """
 
     def __init__(self):
-        self.random_set = set()
+        self.arr = []
+        self.hashmap = {}
         
 
     def insert(self, val: int) -> bool:
         
-        if val in self.random_set:
+        if val in self.hashmap:
             return False
-        else:
-            self.random_set.add(val)
-            return True
+        
+        self.hashmap[val] = len(self.arr)
+        self.arr.append(val)
+        return True
         
 
     def remove(self, val: int) -> bool:
         
-        if val in self.random_set:
-            self.random_set.remove(val)
-            return True
-        
-        else:
+        if val not in self.hashmap:
             return False
+        
+        idx = self.hashmap[val]
+        last = self.arr[-1]
+    
+        self.arr[idx] = last
+        self.hashmap[last] = idx
+        self.arr.pop()
+        del self.hashmap[val]
+        return True
+        
+        
         
 
     def getRandom(self) -> int:
         
-        if self.random_set:
-            return random.choice(list(self.random_set))
+        return random.choice(self.arr)
+        
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:
